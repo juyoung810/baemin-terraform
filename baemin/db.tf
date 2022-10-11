da70fd8c541f0f39cd03f4f65ba060835113386b
juya <kjy2967@naver.com>
2022-10-11 19:53:03 +0900

    modify : db security group only ec2 in

--- baemin/db.tf
+++ baemin/db.tf
@@ -7,26 +7,20 @@ resource "aws_db_subnet_group" "rds" {
     Name = "dbsubnets-baemin-vpc-10-0-0-0"
   }
 }
+# RDS는 private subne에 위치, 인터넷 통해 접근 불가
+# EC2만 RDS에 접근 가능
 resource "aws_security_group" "baemin-rds-sg" {
   name        = "baemin-rds-sg"
   description = "Allow baemin inbound traffic"
   vpc_id      = aws_vpc.baemin-vpc-10-0-0-0.id # 생성한 vpc
 
   ingress {
-    description      = "web from VPC"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1" 
-    cidr_blocks      = ["0.0.0.0/0"]
+    description      = "Allow rds traffic from only ec2 sg"
+    from_port        =  "3306"
+    to_port          =  "3306"
+    protocol         =  "tcp" 
+    security_groups = [aws_security_group.baemin-sg.id]
 
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
@@ -40,14 +34,15 @@ resource "aws_db_instance" "baemin-rds" {
   engine = "postgres"
   engine_version = "12"
   instance_class = "db.t2.micro"
-  name = "baemin_rds"
+  db_name = "baemin-rds"
   username = "ju"
   password = "baeminRDS"
   db_subnet_group_name   = aws_db_subnet_group.rds.name
+  skip_final_snapshot = true # 삭제되기전 snapshot 생성되지 않도록
   vpc_security_group_ids = [ 
     aws_security_group.baemin-sg.id,
    ]
   tags = {
-      "name" = "baemin-rds"
-    }
+      Name = "baemin-rds"
+  }
 }
